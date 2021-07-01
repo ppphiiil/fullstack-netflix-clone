43938d7dfbe9adeb588ebf90a72b88483b94df76
Stan-Getz <stanislav.genunchi@gmail.com>
2021-07-01 16:02:10 +0200

    Added content to Header.js

---
+++ ui/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+import './Header.css';
+
+export default function Header() {
+  return (
+    <div className="header-container">
+      <Navbar />
+    </div>
+  );
+}
