d1be8dc9ce08f1ce68072c1cff026b4098370831
ppphiiil <info@philmeinwelt.com>
2021-07-01 10:23:35 +0200

    created VideoSlider

---
+++ ui/src/components/VideoSlider/VideoSlider.js
@@ -0,0 +1,21 @@
+// import React from 'react'
+// 
+// export default function VideoSlider() {
+//     return (
+// 
+//         <div className="slider">
+//             <div className="slider-image-container">
+//                 <img className="slider-img " src="VideoSlider" alt="image">
+//                     <img className="slider-img" src="images/image4.jpg" alt="image">
+//                         <img className="slider-img " src="images/image1.jpg" alt="image"> 
+//             </div>
+// 
+//             <div className="nav">
+//                 <button id="left" className="navstyle">
+//                     x </button>
+//                 <button id="right" className="navstyle"> x </button>
+// 
+//             </div>
+//         </div>
+//     )
+// }
