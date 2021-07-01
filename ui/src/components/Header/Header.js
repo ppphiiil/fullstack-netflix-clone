f8a9c0ffd45f7a6ad6c28da72a1d93653ad7af2d
Stan-Getz <stanislav.genunchi@gmail.com>
2021-07-01 22:39:00 +0200

    added SearchField component to Header

--- ui/src/components/Header/Header.js
+++ ui/src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
+import SearchField from '../SearchField/SearchField';
 import './Header.css';
+import '../SearchField/SearchField.css';
 
 export default function Header() {
   return (
     <div className="header-container">
       <Navbar />
+      <SearchField />
     </div>
   );
 }
