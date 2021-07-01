e22bf254a230c0e41a52c408f372aa6dcf9e99d9
Stan-Getz <stanislav.genunchi@gmail.com>
2021-07-01 16:01:15 +0200

    Removed "My React App" note

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import '../src/App.css';
-import Header from './components/Header';
-import SearchBar from './components/Search';
+import Header from './components/Header/Header';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <h1>My React App</h1>
       <Header />
-      <SearchBar />
       <Footer />
     </div>
   );
