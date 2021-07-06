52c86a9d09097b21f274c03babc44a01c5c3ddc9
Dusan <dusan.chmelko@yahoo.com>
2021-07-06 23:00:35 +0200

    add video route and test player in index.html

--- api/app.js
+++ api/app.js
@@ -1,14 +1,16 @@
 const express = require("express");
 require("./mongoConnect");
-// require("./connect");
 const cors = require("cors");
 
 const seriesRoute = require("./routes/seriesRoutes");
 const rowRoute = require("./routes/rowRoutes");
+const videoRoute = require("./routes/videoRoutes");
 
 const port = 3000;
 
 const app = express();
+
+app.use(express.static("public"));
 
 app.use(express.json());
 
@@ -16,5 +18,6 @@ app.use(cors());
 
 app.use("/series", seriesRoute);
 app.use("/series/row", rowRoute);
+app.use("/video", videoRoute);
 
 app.listen(port, () => console.log("server running on port: ", port));
