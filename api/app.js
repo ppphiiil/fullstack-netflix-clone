8ad4ac19b56ae50ada0c5c1fc46424cdb7b26a28
ppphiiil <info@philmeinwelt.com>
2021-07-08 12:19:18 +0200

    for presentation

--- api/app.js
+++ api/app.js
@@ -3,11 +3,11 @@ require( "./mongoConnect" );
 const cors = require( "cors" );
 
 
-const seriesRoute = require("./routes/seriesRoutes");
-const rowRoute = require("./routes/rowRoutes");
-const videoRoute = require("./routes/videoRoutes");
+const seriesRoute = require( "./routes/seriesRoutes" );
+const rowRoute = require( "./routes/rowRoutes" );
+const videoRoute = require( "./routes/videoRoutes" );
 
-const port = 3000;
+const port = 3500;
 
 const app = express();
 
@@ -17,8 +17,8 @@ app.use( express.json() );
 
 app.use( cors( { origin: "*" } ) );
 
-app.use("/series", seriesRoute);
-app.use("/series/row", rowRoute);
-app.use("/video", videoRoute);
+app.use( "/series", seriesRoute );
+app.use( "/series/row", rowRoute );
+app.use( "/video", videoRoute );
 
 app.listen( port, () => console.log( "server running on port: ", port ) );
